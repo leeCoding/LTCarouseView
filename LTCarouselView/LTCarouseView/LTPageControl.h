5f649b2d6e8a97aec5b854ebb7585681f8deecc2
Jonny_Lee <nscode@sina.com>
2016-11-01 10:18:02 +0800

    add file

---
+++ LTCarouselView/LTCarouseView/LTPageControl.h
@@ -0,0 +1,13 @@
+//
+//  LTPageControl.h
+//  LTProject
+//
+//  Created by Jonny on 2016/10/14.
+//  Copyright © 2016年 ZUBMO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTPageControl : UIPageControl
+
+@end
---
+++ LTCarouselView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LTCarouselView
+//
+//  Created by Jonny on 2016/11/1.
+//  Copyright © 2016年 Jonny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
